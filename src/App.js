4aca8be902ae57fad024ba3848a23b07af6904f7
Gaurav Rao <gauravraosahab@gmail.com>
2021-11-03 12:45:18 -0300

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./app/Nav";
+import Contact from "./app/components/ContactUs";
+import Home from "./app/components/Home";
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Switch,
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Nav />
+                <Switch>
+                    <Route path="/contact" component={Contact}/>
+                    <Route path="/" exact component={Home}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
